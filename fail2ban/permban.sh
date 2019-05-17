87672df9beb34940fd9ea80d9effbee30221bde7
John Mc <john@softsmart.co.za>
2019-05-17 21:55:22 +0200

    Fail2ban is working again

---
+++ fail2ban/permban.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+x=$(pgrep permban.sh | wc -w)
+if [ $x -gt 2 ]; then
+        exit
+fi
+
+
+Password=`/usr/webcp/get_password.sh`
+
+
+for FullFileName in /var/www/html/webcp/nm/*.permban;
+do
+	##echo $FullFileName
+        x=$FullFileName
+        echo "x: '$x'"
+        y=${x%.permban}
+        echo "y: '$y'"
+        ip=${y##*/}
+        echo "ip: '$ip'"
+
+	ip=${ip//_/\/}
+        echo "ip: '$ip'"
+
+	if [ "$ip" != "*" ]
+	then
+		/usr/sbin/ufw deny from $ip
+		$(mysql cpadmin -u root -p${Password} -se "INSERT INTO fail2ban VALUES (0, '$ip', '', '', '', '*', 0, 'inout', '$(date +\%Y-\%m-\%d\ \%H:\%M:\%S)', 0, 'perm', 'perm', 'perm');")
+		rm -fr $FullFileName
+	fi
+
+done
+
