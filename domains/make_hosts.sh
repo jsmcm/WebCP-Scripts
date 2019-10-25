763713742261b10a68615edf1f3f57b08e993f8c
John Mc <john@softsmart.co.za>
2019-10-25 11:04:16 +0200

    add new domains to the /etc/hosts file

---
+++ domains/make_hosts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+Password=`/usr/webcp/get_password.sh`
+domainName=`hostname`
+
+echo "127.0.1.1 $domainName" > /etc/hosts
+echo "127.0.0.1 localhost" >> /etc/hosts
+
+for nextDomain in $(mysql cpadmin -u root -p${Password} -se "SELECT fqdn FROM domains WHERE deleted = 0;")
+do
+	echo "127.0.1.1 $nextDomain" >> /etc/hosts
+done
