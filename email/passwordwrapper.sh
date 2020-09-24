b682f5f76b82fb0a6f9d9c5bc763789516254a17
john <john@softsmart.co.za>
2020-09-24 12:01:19 +0200

    3.0.2

--- email/passwordwrapper.sh
+++ email/passwordwrapper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#mkpassword.sh is called from mkemail.sh so no need for this, it causes double entries in the password files
+
 x=$(pgrep passwordwrapper.sh | wc -w)
 if [ $x -gt 2 ]; then
         exit
@@ -15,21 +17,21 @@ do
 	if [ -f $FullFileName ]
 	then	
 
-		echo $FullFileName
-		x=$FullFileName
-		y=${x%.mailpassword}
-		UserName=${y##*/}
-               	UserName=${UserName%_*}
-                DomainName=$(mysql cpadmin -u root -p${Password} -se "SELECT fqdn FROM domains WHERE deleted = 0 AND UserName = '$UserName' AND domain_type = 'primary';")
-                GroupID=$(mysql cpadmin -u root -p${Password} -se "SELECT Gid FROM domains WHERE deleted = 0 AND UserName = '$UserName' AND domain_type = 'primary';")
-                UserID=$(mysql cpadmin -u root -p${Password} -se "SELECT Uid FROM domains WHERE deleted = 0 AND UserName = '$UserName' AND domain_type = 'primary';")
+		#echo $FullFileName
+		#x=$FullFileName
+		#y=${x%.mailpassword}
+		#UserName=${y##*/}
+               	#UserName=${UserName%_*}
+                #DomainName=$(mysql cpadmin -u root -p${Password} -se "SELECT fqdn FROM domains WHERE deleted = 0 AND UserName = '$UserName' AND domain_type = 'primary';")
+                #GroupID=$(mysql cpadmin -u root -p${Password} -se "SELECT Gid FROM domains WHERE deleted = 0 AND UserName = '$UserName' AND domain_type = 'primary';")
+                #UserID=$(mysql cpadmin -u root -p${Password} -se "SELECT Uid FROM domains WHERE deleted = 0 AND UserName = '$UserName' AND domain_type = 'primary';")
 
-		echo "DomainName: $DomainName"
-		echo "UserName: $UserName"
-		echo "Guid: $GroupID"
-		echo "Uid: $UserID"
+		#echo "DomainName: $DomainName"
+		#echo "UserName: $UserName"
+		#echo "Guid: $GroupID"
+		#echo "Uid: $UserID"
 		
-		/usr/webcp/email/mkpasswdfiles.sh $DomainName $UserName $GroupID $UserID				
+		#/usr/webcp/email/mkpasswdfiles.sh $DomainName $UserName $GroupID $UserID				
 		rm -fr $FullFileName
 				
 	fi
